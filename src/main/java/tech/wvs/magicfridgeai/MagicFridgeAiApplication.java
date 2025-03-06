1c8e00cad8fae6f3b862415c66b59fda6526f0c7
wallacevilela7 <wallace.vilela9@gmail.com>
2025-03-06 17:55:58 -0300

    Create project

---
+++ src/main/java/tech/wvs/magicfridgeai/MagicFridgeAiApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.magicfridgeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicFridgeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicFridgeAiApplication.class, args);
+    }
+
+}
