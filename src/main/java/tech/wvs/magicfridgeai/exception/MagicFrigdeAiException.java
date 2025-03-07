bedd8eee2ca2e143ecfc5174e076f70290f23e5f
wallacevilela7 <wallace.vilela9@gmail.com>
2025-03-07 11:50:45 -0300

    Adiciona a classe MagicFrigdeAiException para tratamento de exceções personalizadas

---
+++ src/main/java/tech/wvs/magicfridgeai/exception/MagicFrigdeAiException.java
@@ -0,0 +1,7 @@
+package tech.wvs.magicfridgeai.exception;
+
+public class MagicFrigdeAiException extends RuntimeException {
+    public MagicFrigdeAiException(String message) {
+        super(message);
+    }
+}
